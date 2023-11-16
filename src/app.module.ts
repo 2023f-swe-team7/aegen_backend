266aa77195ad0f3ae8c618f30501bb7f67a34061
chanyeong <jcy030896@naver.com>
2023-11-16 22:36:53 +0900

    feat: mail module init

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from './config/app/config.module';
 import { AuthModule } from './auth/auth.module';
+import { MailModule } from './mail/mail.module';
 
 @Module({
-  imports: [AppConfigModule, AuthModule],
+  imports: [AppConfigModule, AuthModule, MailModule],
   controllers: [AppController],
   providers: [AppService],
 })

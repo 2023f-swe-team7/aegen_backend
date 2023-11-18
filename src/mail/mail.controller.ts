0f20be8ec5e1addb8cd8c9df42c1bde95cefa6a2
chanyeong <jcy030896@naver.com>
2023-11-18 12:31:08 +0900

    feat: 메일 생성 로직 작성

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, UseGuards } from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { MailService } from './mail.service';
+import { CurrentUser } from 'src/common/decorator/current-user.decorator';
+import { User } from '@prisma/client';
+import { GetMailDto, GetMailResponse } from './dtos/get-mail.dto';
+import { CommonResponseDto } from 'src/common/dtos/common-response.dto';
 
+@UseGuards(JwtGuard)
 @Controller('mail')
-export class MailController {}
+export class MailController {
+  constructor(private readonly mailService: MailService) {}
+
+  @Get()
+  async getMail(@CurrentUser() user: User, @Body() getMailDto: GetMailDto) {
+    const newMail = await this.mailService.getMail(user, getMailDto);
+    return new CommonResponseDto(newMail);
+  }
+}

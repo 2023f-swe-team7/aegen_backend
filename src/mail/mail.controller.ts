1421a62ad521cf6f6ef8d0780a6d823f08a570f5
dodoandchichi <100553243+dodoandchichi@users.noreply.github.com>
2023-11-19 15:12:35 +0900

    GMail을 통한 메일 전송 구현
    구글 메일 API 연동
    send-mail.dto.ts
    mail.controller.ts
    mail.service.ts

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+=======
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+>>>>>>> Stashed changes
 import { JwtGuard } from 'src/auth/guards/jwt.guard';
 import { MailService } from './mail.service';
 import { CurrentUser } from 'src/common/decorator/current-user.decorator';
@@ -6,14 +10,21 @@ import { User } from '@prisma/client';
 import { GetMailDto } from './dtos/get-mail.dto';
 import { CommonResponseDto } from 'src/common/dtos/common-response.dto';
 
-@UseGuards(JwtGuard)
-@Controller('mail')
+
+@Controller('mail/')
 export class MailController {
-  constructor(private readonly mailService: MailService) {}
+  constructor(
+    private readonly mailService: MailService
+    ) {}
 
   @Get()
   async getMail(@CurrentUser() user: User, @Query() getMailDto: GetMailDto) {
     const newMail = await this.mailService.getMail(user, getMailDto);
     return new CommonResponseDto(newMail);
+  }
+
+  @Post('send')
+  async sendMail(@Body() body: { receiver: string; subject: string; text: string }) {
+    return await this.mailService.sendEmail(body.receiver, body.subject, body.text);
   }
 }
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -2,6 +2,8 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { Subject, User } from '@prisma/client';
 import { PrismaService } from 'src/config/database/prisma.service';
 import { GetMailDto, GetMailResponse } from './dtos/get-mail.dto';
+import { google } from 'googleapis';
+import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class MailService {
@@ -35,5 +37,57 @@ export class MailService {
     tmpMail = tmpMail.replace(/professor_name/gi, subjectInfo.professorName);
     tmpMail = tmpMail.replace(/rn/gi, '\\n');
     return tmpMail;
+  }
+
+  // OAuth2Client를 구성합니다.
+  private oauth2Client = new google.auth.OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    process.env.REDIRECT_URL
+  );
+    
+  async sendEmail(receiver: string, subject: string, text: string) {
+    this.oauth2Client.setCredentials({
+      refresh_token: process.env.REFRESH_TOKEN,
+    });
+
+    console.log('OAuth2Client 설정:', this.oauth2Client);
+    console.log('이메일 전송 설정:', {
+      receiver,
+      subject,
+      text,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+      accessToken: process.env.ACCESS_TOKEN,
+    });
+
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: 'kimdozz01@gmail.com',
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
+        accessToken: process.env.ACCESS_TOKEN,
+      },
+    });
+
+    const mailOptions = {
+      from: 'kimdozz01@gmail.com',
+      to: receiver, 
+      subject: subject, 
+      text: text, 
+    };
+
+    try {
+      const result = await transporter.sendMail(mailOptions);
+      console.log('이메일 전송 결과:', result);
+      return result;
+    } catch (error) {
+      console.error('이메일 전송 중 오류 발생:', error);
+      throw error; // 오류를 다시 throw하여 호출하는 쪽에서도 알 수 있게 함
+    }
   }
 }
